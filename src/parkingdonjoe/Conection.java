57d7d33e25cf1dc99e8dc12d884b506e3a950867
oalvarez1990 <omaraleyser@hotmail.com>
2022-05-22 15:56:20 -0500

    Parking don joe

---
+++ src/parkingdonjoe/Conection.java
@@ -0,0 +1,17 @@
+
+package parkingdonjoe;
+import java.sql.*;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Conection {
+    public String usuario ="";
+    public String pass = "";
+    public String url= "jdbc:sqlserver://;database=parkingonjoe;integratedSecurity=true;\"";
+    
+    
+}
